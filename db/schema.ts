46748a1009c05a22c2283ccbaa0b61d1d6b220e2
n1sp <n1sp.pc001@gmail.com>
2025-09-30 08:20:10 +0900

    fix:setting cookieセクションのuser.id:stringに合わせるため、id列をautoincrementのnumberからuuid生成のstringに変更

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,9 @@
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
 
 export const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
   password: text('password').notNull(),
